f041b50173e355a4c50da245c3778370d554d740
Mantas Razanta <mantas.razanta@codeacademylt.onmicrosoft.com>
2022-09-21 19:41:26 +0300

    Add project files.

---
+++ Lesson8_02_03/Program.cs
@@ -0,0 +1,8 @@
+﻿
+using Lesson8_02_03;
+
+Point taskas = new Point(10, 10);
+Point virsusKaire = new Point(0, 0);
+Point apaciaDesine = new Point(10, 10);
+Rectangle staciakampis = new Rectangle(virsusKaire, apaciaDesine);
+Console.WriteLine(staciakampis.IsPointInRectangle(taskas));
---
+++ Lesson8_02_03/Rectangle.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson8_02_03
+{
+    internal class Rectangle
+    {
+        public Point TopLeft;
+        public Point BottomRight;
+
+        public Rectangle(Point topLeft, Point bottomRight)
+        {
+            TopLeft = topLeft;
+            BottomRight = bottomRight;
+        }
+
+        public bool IsPointInRectangle(Point taskas)
+        {
+            if (taskas.X >= TopLeft.X && taskas.X <= BottomRight.X &&
+                taskas.Y >= TopLeft.Y && taskas.Y <= BottomRight.Y)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            
+        }
+    }
+}
